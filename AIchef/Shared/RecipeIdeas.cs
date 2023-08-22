67d1528c931f22b51c7cbfef5d0c9f98ff683308
tj <taidjerobinson@gmail.com>
2023-08-22 00:51:11 -0700

    start recipe idea page

---
+++ AIchef/Shared/RecipeIdeas.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AIchef.Shared
+{
+	public class Idea
+	{
+		public int? index { get; set; }
+		public string? title { get; set; }
+		public string? description { get; set; }
+	}
+}
+
